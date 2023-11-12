0bdd751bcfedd6554e358da77c6100af80c29735
Armando Peña Tamayo <arman2p284@gmail.com>
2023-11-12 00:24:39 -0500

    Fix: Changes route to go to work with bash

--- gp.sh
+++ gp.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Set the absolute path to the Go executable
-GO_EXECUTABLE="C:\Program Files\Go\bin"
+GO_EXECUTABLE="\"/mnt/c/Program Files/Go/bin/go.exe\""
 
 # Run tests
-$GO_EXECUTABLE test ./...
+eval $GO_EXECUTABLE test ./...
 
 # Build the executable
-$GO_EXECUTABLE build -o ./output/wkgo.exe ./...
+eval $GO_EXECUTABLE build -o ./output/wkgo.exe ./...
 
 # Check if the build was successful
 if [ $? -eq 0 ]; then
